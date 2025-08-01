f6a8c73498a7160f8ae2901b3c5536d0586212d5
Andrew Lou <andrewloupro@gmail.com>
2025-08-01 13:27:14 -0400

    Initial commit -m Fish weight project

---
+++ day5/fish_weight_project.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Ridge,Lasso
+from sklearn.preprocessing import PolynomialFeatures
+
+from sklearn.metrics import mean_squared_error
+
+train_feature = pd.read_csv('https://raw.githubusercontent.com/rugvedmhatre/NYU-ML-2024-Session-1/main/day5/fish_market_feature.csv')
+train_label = pd.read_csv('https://raw.githubusercontent.com/rugvedmhatre/NYU-ML-2024-Session-1/main/day5/fish_market_label.csv')
+X_train = train_feature.values
+y_train = train_label.values
+
+
+test_feature = pd.read_csv('https://raw.githubusercontent.com/rugvedmhatre/NYU-ML-2024-Session-1/main/day5/fish_market_test_feature.csv')
+test_label = pd.read_csv('https://raw.githubusercontent.com/rugvedmhatre/NYU-ML-2024-Session-1/main/day5/fish_market_test_label.csv')
+X_test = test_feature.values
+y_test = test_label.values
+
+
+i = 3
+poly = PolynomialFeatures(degree=i, include_bias=False)
+design_matrix_train = poly.fit_transform(X_train)
+design_matrix_test = poly.transform(X_test)
+
+model = Ridge(fit_intercept=True, alpha=0.1)
+model.fit(design_matrix_train, y_train)
+
+y_hat_train = model.predict(design_matrix_train)
+y_hat_test = model.predict(design_matrix_test)
+
+print(mean_squared_error(y_train, y_hat_train))
+print(mean_squared_error(y_test, y_hat_test))
+
+#print(np.mean((y_train - y_hat_train) ** 2))
+#print(np.mean((y_test - y_hat_test) ** 2))
