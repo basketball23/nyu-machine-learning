dadb55141bc3f0ea80796b919a11d68093fcc3d4
Andrew Lou <andrewloupro@gmail.com>
2025-08-01 13:40:03 -0400

    Turns out any MSE under 10k is good

--- day5/fish_weight_project.py
+++ day5/fish_weight_project.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import Ridge,Lasso
 from sklearn.preprocessing import PolynomialFeatures
 
+from sklearn.metrics import mean_squared_error
 
 train_feature = pd.read_csv('https://raw.githubusercontent.com/rugvedmhatre/NYU-ML-2024-Session-1/main/day5/fish_market_feature.csv')
 train_label = pd.read_csv('https://raw.githubusercontent.com/rugvedmhatre/NYU-ML-2024-Session-1/main/day5/fish_market_label.csv')
@@ -17,23 +18,17 @@ X_test = test_feature.values
 y_test = test_label.values
 
 
-i = 3
+i = 2
+
 poly = PolynomialFeatures(degree=i, include_bias=False)
 design_matrix_train = poly.fit_transform(X_train)
 design_matrix_test = poly.transform(X_test)
-'''
+
 model = Ridge(fit_intercept=True, alpha=0.1)
 model.fit(design_matrix_train, y_train)
 
 y_hat_train = model.predict(design_matrix_train)
 y_hat_test = model.predict(design_matrix_test)
-'''
 
-W = np.linalg.inv(design_matrix_train.T @ design_matrix_train) @ design_matrix_train.T @ y_train
-
-y_hat_train = design_matrix_train @ W
-y_hat_test = design_matrix_test @ W
-
-
-print(np.mean((y_train - y_hat_train) ** 2))
-print(np.mean((y_test - y_hat_test) ** 2))
+print(mean_squared_error(y_train, y_hat_train))
+print(mean_squared_error(y_test, y_hat_test))
